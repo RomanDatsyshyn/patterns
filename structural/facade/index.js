6c927687b631c049e176aab3e77991ae9762a1c7
Roman Datsyshyn <roman.datsyshyn@hotel-spider.com>
2022-08-04 10:34:01 +0300

    added facade

---
+++ structural/facade/index.js
@@ -0,0 +1,49 @@
+var Conveyor = /** @class */ (function () {
+    function Conveyor() {
+    }
+    Conveyor.prototype.setBody = function () {
+        console.log('Body set!');
+    };
+    Conveyor.prototype.getEngine = function () {
+        console.log('Dismantle Engine!');
+    };
+    Conveyor.prototype.setEngine = function () {
+        console.log('Engine set!');
+    };
+    Conveyor.prototype.setInterior = function () {
+        console.log('Exterior added!');
+    };
+    Conveyor.prototype.changeInterior = function () {
+        console.log('Update interior!');
+    };
+    Conveyor.prototype.setExterior = function () {
+        console.log('Added interior!');
+    };
+    Conveyor.prototype.setWheels = function () {
+        console.log('Wheels!');
+    };
+    Conveyor.prototype.addElectronics = function () {
+        console.log('Added electronics!');
+    };
+    Conveyor.prototype.paint = function () {
+        console.log('Car painted!');
+    };
+    return Conveyor;
+}());
+var ConveyorFacade = /** @class */ (function () {
+    function ConveyorFacade(car) {
+        this.car = car;
+    }
+    ConveyorFacade.prototype.assembleCar = function () {
+        this.car.setBody();
+        this.car.setEngine();
+        this.car.setInterior();
+        this.car.setExterior();
+        this.car.setWheels();
+        this.car.addElectronics();
+        this.car.paint();
+    };
+    return ConveyorFacade;
+}());
+var conveyor = new ConveyorFacade(new Conveyor());
+conveyor.assembleCar();
---
+++ structural/facade/index.ts
@@ -0,0 +1,59 @@
+class Conveyor {
+
+	setBody() {
+		console.log('Body set!');
+	}
+
+	getEngine() {
+        console.log('Dismantle Engine!');
+    }
+
+	setEngine() {
+		console.log('Engine set!');
+	}
+
+	setInterior() {
+		console.log('Exterior added!');
+	}
+
+	changeInterior() {
+		console.log('Update interior!');
+	}
+
+	setExterior() {
+		console.log('Added interior!');
+	}
+
+	setWheels() {
+		console.log('Wheels!');
+	}
+
+	addElectronics() {
+		console.log('Added electronics!');
+	}
+
+	paint() {
+		console.log('Car painted!');
+	}
+}
+
+class ConveyorFacade {
+    car: any;
+
+	constructor(car:Conveyor) {
+		this.car = car;
+	}
+
+	assembleCar() {
+		this.car.setBody();
+		this.car.setEngine();
+		this.car.setInterior();
+		this.car.setExterior();
+		this.car.setWheels();
+		this.car.addElectronics();
+		this.car.paint();
+	}
+}
+
+const conveyor = new ConveyorFacade(new Conveyor());
+conveyor.assembleCar();
